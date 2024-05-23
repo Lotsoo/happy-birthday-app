889422d3be307bedb39de7f62e06cd8d55499b03
lotsoo <braindeathrama@gmail.com>
2024-05-23 14:44:29 +0700

    Mengubah opasitas

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -76,6 +76,7 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
             painter = image,
             contentDescription = null,
             contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         GreetingText(
             message = message,
